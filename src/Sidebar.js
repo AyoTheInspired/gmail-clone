f055dc060e1345c5f63b54bd74a52b7e7ae3253e
Ayo <bimboladxtreme@gmail.com>
2021-08-07 19:04:03 +0100

    Added more Sidebar Options

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,11 @@ import AddIcon from "@material-ui/icons/Add";
 import SidebarOption from "./SidebarOption";
 import InboxIcon from "@material-ui/icons/Inbox";
 import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Sidebar() {
 	return (
@@ -22,8 +27,11 @@ function Sidebar() {
 				selected={true}
 			/>
 			<SidebarOption Icon={StarIcon} title="Starred" number={54} />
-			<SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
-			<SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+			<SidebarOption Icon={InboxIcon} title="Snoozed" number={54} />
+			<SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
+			<SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
+			<SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
+			<SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
 		</Div>
 	);
 }
