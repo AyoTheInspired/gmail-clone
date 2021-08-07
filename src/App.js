fb99609d22deccb3a69681965634206214e4464b
Ayo <bimboladxtreme@gmail.com>
2021-08-07 15:27:25 +0100

    Added Header file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Header from "./Header";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="app">
 			<h2>THE GMAIL CLONE</h2>
+			<Header />
 		</div>
 	);
 }
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+	return <Div>THE HEADER</Div>;
+}
+
+export default Header;
+
+const Div = styled.div``;
